0d49ba66c47653eba7dd9cc0dfbf9da66cd258d7
Stanislav Efimtsev <evendot@yandex.ru>
2023-04-26 12:19:52 +0300

    Registration is ready

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { validationResult } from 'express-validator';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
-import {registerValidation} from './validations/auth.js';
+import { registerValidation } from './validations/auth.js';
 import UserModel from './models/User.js';
 import bcrypt from 'bcrypt';
 
@@ -11,8 +11,8 @@ const port = 8080; // Порт
 
 // Подключение к базе данных MongoDB
 mongoose.connect("mongodb://admin:1q2w3e4r@127.0.0.1:27017/restaurant",)
-.then(() => {console.log('DB is ok')})
-.catch(() => console.log('DB error', err));
+    .then(() => { console.log('DB is ok') })
+    .catch(() => console.log('DB error', err));
 
 // Подключение Express
 const app = express();
@@ -25,37 +25,64 @@ app.get('/', (req, res) => {
 });
 
 app.post('/auth/register', registerValidation, async (req, res) => {
-    // Валидация данных
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json(errors.array());
-    }
+    try {
+        // Валидация данных
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array());
+        }
 
-    // Шифрование пароля
-    const password = req.body.password;
-    if (password === undefined || password === null){
-        return "password is undefined or null";
-    }
-    
-    const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt);
+        // Шифрование пароля
+        const password = req.body.password;
+        if (password === undefined || password === null) {
+            return "password is undefined or null";
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
 
-    const doc = new UserModel({
+        // Создание документа - модели пользователя
+        const doc = new UserModel({
             fullName: req.body.fullName,
             email: req.body.email,
-            passwordHash,
+            passwordHash: hash,
             avatarUrl: req.body.avatarUrl,
-    });
- 
-    const user = await doc.save();
+        });
 
-    res.json(user);
+        // Сохранение документа
+        const user = await doc.save();
+
+        // Шифрование в jwt-токене id пользователя
+        const token = jwt.sign(
+            {
+                _id: user._id
+            },
+            'secret123',
+            {
+                expiresIn: '10d'
+            },
+        );
+
+        // Деструктуризация для того, чтобы убрать passwordHash из вывода
+        const {passwordHash, ...userData} = user._doc;
+
+        res.json({
+            ...userData,
+            token
+        });
+    } catch (error) {
+        console.log(error);
+        res.json({
+            message: "Registration is failed",
+            error,
+        });
+    }
+
 });
 // Авторизация
 app.post('/auth/login', (req, res) => {
     // Создание jwt токена
     const token = jwt.sign(
-        { 
+        {
             email: req.body.email,
             fullName: req.body.name,
         },
@@ -71,11 +98,11 @@ app.post('/auth/login', (req, res) => {
 
 
 // Запуск сервера при отсутствии ошибок
-app.listen(port, (err) => { 
-    if(err){
+app.listen(port, (err) => {
+    if (err) {
         return console.log(err);
     }
-    else{
+    else {
         console.log(`Сервер работает по адресу: http://${hostname}:${port}/`);
     }
 
