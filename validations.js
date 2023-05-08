5de77f3e0c4e7d4f2321a920270f74883b1f161f
Stanislav Efimtsev <evendot@yandex.ru>
2023-05-08 21:48:18 +0300

    Validation fixes

--- validations.js
+++ validations.js
@@ -11,15 +11,15 @@ export const registerValidation = [
 /** Валидация авторизации */
 export const loginValidation = [
     body('email', 'The email must be in proper format!').isEmail(),
-    body('password', 'The password\'s length must be at least 2 characters!').isLength({ min: 5}),
+    body('password', 'The password\'s length must be at least 5 characters!').isLength({ min: 5}),
 ];
 
 /** Валидация создания блюда */
 export const dishCreateValidation = [
     body('title', 'The title\'s length must be at least 2 characters!').isLength({min: 2}).isString(),
-    body('cookingtime', 'The cooking time must be a float number!').isInt(),
-    body('description', 'The description\'s length must be at least 10 characters!').isLength({min: 5}).isString(),
-    body('imageUrl', 'The image must be a URL!').optional().isURL(),
-    body('ingredients', 'The ingredients must be a string!').optional().isArray(),
-    body('tags', 'The tags enumeration must be a string!').optional().isArray()
+    body('cookingtime', 'The cooking time must be an integer!').isInt(),
+    body('description', 'The description\'s length must be at least 10 characters!').isLength({min: 10}).isString(),
+    body('imageUrl', 'The image must be a string!').optional().isString(),
+    body('ingredients', 'The ingredients must be an array!').optional().isArray(),
+    body('tags', 'The tags must be an array!').optional().isArray()
 ];
